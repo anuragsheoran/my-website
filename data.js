fa75691d10abc7d42ef7f535d06fb719743dff63
anuragsheoran <anuragsheoran25@gmail.com>
2025-11-19 23:20:11 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -20,5 +20,23 @@ const moviesData = [
             link: "https://pub-58c37aacc5ca4f78ba86b47195edec29.r2.dev/Param.Sundari.2025.720p.HEVC.Hindi.WEB-DL.ESub.x265-%20moviesdrives.cv.mkv?token=1763547127142"
         }
     }
+    // --------------------
+    // JOLLY LLB 2013
+    // --------------------
+    {
+        title: "Jolly LLB",
+        category: "Bollywood",
+        poster: "posters/jolly_llb.jpg"
+
+        quality1080p: {
+            size: "3.73GB",
+            link: "https://pub-2fe28ab4d73e444bb8b6de678d39a70d.r2.dev/3e7f283a13e8ee60243f8d3110bf385e?token=1763574122112"
+        },
+
+        quality720p: {
+            size: "1.12MB",
+            link: "https://pub-58c37aacc5ca4f78ba86b47195edec29.r2.dev/Jolly%20LLB%202013%20BluRay%20720p%20Hindi%20AAC%205.1%20x264%20ESub%20-%20%20moviesdrives.co.mkv?token=1763571646110"
+        }
+    }
 
 ];
