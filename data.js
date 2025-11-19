1cc70893109c07b2f9bf018d20f4ddd0952ac819
anurag <anuragsheoran25@gmail.com>
2025-11-19 23:58:41 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -38,6 +38,25 @@ const moviesData = [
             size: "1.12GB",
             link: "https://pub-58c37aacc5ca4f78ba86b47195edec29.r2.dev/Jolly%20LLB%202013%20BluRay%20720p%20Hindi%20AAC%205.1%20x264%20ESub%20-%20%20moviesdrives.co.mkv?token=1763571646110"
         }
+    },
+
+    // --------------------
+    // INTERSTELLAR 2014
+    // --------------------
+    {
+        title: "Interstellar [HINDI]",
+        category: "Hollywood",
+        poster: "posters/interstellar.jpg",
+
+        quality1080p: {
+            size: "2.81GB",
+            link: "https://xpe9.com/sm_b2_ygbgxry90i72.html?zoneid=6973312&ymid=176357631809990TINTV62409R40d6R1013Rd8bR8000RR6Vc7776&sourceid=10553224-1560383409-4269441498&tt=2"
+        },
+
+        quality720p: {
+            size: "1.55GB",
+            link: "https://rirki.com/i3CR5GtcXOtLaknWK/128897?param_5=176357635909990TINTV62409R40d6R1013Rd8bR8000RR6Vfec97&param_4=10553224-1560383409-4269441498&zoneid=128897&tt=2"
+        }
     }
 
 ];
