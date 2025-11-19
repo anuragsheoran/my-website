f8d2f90d080656b69828958b0dce0cbfba00beb3
anurag <anuragsheoran25@gmail.com>
2025-11-19 23:45:15 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const moviesData = [
     {
         title: "Param Sundari",
         category: "Bollywood",
-        poster: "posters/param_sundari.jpg",  // Replace with a real poster anytime
+        poster: "posters/param_sundari.jpg",
 
         quality1080p: {
             size: "1.82GB",
@@ -19,14 +19,15 @@ const moviesData = [
             size: "794MB",
             link: "https://pub-58c37aacc5ca4f78ba86b47195edec29.r2.dev/Param.Sundari.2025.720p.HEVC.Hindi.WEB-DL.ESub.x265-%20moviesdrives.cv.mkv?token=1763547127142"
         }
-    }
+    },
+
     // --------------------
     // JOLLY LLB 2013
     // --------------------
     {
         title: "Jolly LLB",
         category: "Bollywood",
-        poster: "posters/jolly_llb.jpg"
+        poster: "posters/jolly_llb.jpg",
 
         quality1080p: {
             size: "3.73GB",
@@ -34,7 +35,7 @@ const moviesData = [
         },
 
         quality720p: {
-            size: "1.12MB",
+            size: "1.12GB",
             link: "https://pub-58c37aacc5ca4f78ba86b47195edec29.r2.dev/Jolly%20LLB%202013%20BluRay%20720p%20Hindi%20AAC%205.1%20x264%20ESub%20-%20%20moviesdrives.co.mkv?token=1763571646110"
         }
     }
